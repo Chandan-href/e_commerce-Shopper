4a74ed4f48a19351b8210c4967be4da7988c8c6c
chandan verma <july2008verma@gmaol.com>
2026-09-12 20:57:59 +0530

    Update SHOPPER project and add Nodemailer

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
+    <div className="flex items-center justify-between  font-medium">
       <Link to="/">
         <img src={assets.logo} className="w-40" alt="" />
       </Link>
